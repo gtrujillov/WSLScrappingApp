84bbe9a807b5e917462f7f2c1fe28f7dd025ccf1
Gonzalo Trujillo Vallejo <gonzalo.trujillo@babelgroup.com>
2025-01-25 17:49:53 +0100

    [WSL-12] Fetch athletes data

---
+++ WSLScrappingApp/WSLScrappingApp/Services/ScrapingService.swift
@@ -0,0 +1,35 @@
+//
+//  ScrapingService.swift
+//  WSLScrappingApp
+//
+//  Created by Gonzalo Trujillo Vallejo on 25/1/25.
+//
+
+import Foundation
+import SwiftSoup
+
+class ScrapingService {
+    func fetchHTML(from url: String) async throws -> Document {
+        guard let url = URL(string: url) else {
+            throw NSError(domain: "Invalid URL", code: 1001, userInfo: nil)
+        }
+        let data = try Data(contentsOf: url)
+        return try SwiftSoup.parse(String(data: data, encoding: .utf8)!)
+    }
+    
+    func parseAthletes(from document: Document) -> [AthleteModel] {
+        var athletes: [AthleteModel] = []
+        
+        let ahtletesElements: Elements = try! document.select(".athletes-directory-module__body .tooltip-item")
+        
+        for athlete in ahtletesElements {
+            let name = try! athlete.select(".athlete-name").text()
+            let country = try! athlete.select(".athlete-country-name").text()
+            let category = try! athlete.select(".avatar-text-tertiary").text()
+            let athlete = AthleteModel(id: UUID(), name: name, country: country, category: category)
+            athletes.append(athlete)
+        }
+        
+        return athletes
+    }
+}
---
+++ WSLScrappingApp/WSLScrappingApp/Views/AthletesView.swift
@@ -0,0 +1,46 @@
+//
+//  AthletesView.swift
+//  WSLScrappingApp
+//
+//  Created by Gonzalo Trujillo Vallejo on 25/1/25.
+//
+
+import SwiftUI
+
+struct AthletesView: View {
+    
+    @StateObject private var viewModel =  AthletesViewModel()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                if viewModel.isLoading {
+                    ProgressView("Cargando atletas...") // Indicador de carga
+                        .progressViewStyle(CircularProgressViewStyle())
+                        .padding()
+                } else {
+                    List(viewModel.athletes, id: \.name) { athlete in
+                        VStack(alignment: .leading) {
+                            Text(athlete.name)
+                                .font(.headline)
+                            Text(athlete.country)
+                                .font(.subheadline)
+                        }
+                        .padding(.vertical, 8)
+                    }
+                }
+            }
+            .navigationTitle("Atletas de WSL")
+            .onAppear {
+                // Llamamos a la función loadAthletes cuando la vista aparece
+                Task {
+                    await viewModel.loadAthletes()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AthletesView()
+}
